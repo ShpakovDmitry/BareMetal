86a2f52ed8c8aa68e7f272fc9697b1216dd51c2d
Dmitry <work.dmitry.shpakov@gmail.com>
2020-08-06 22:37:29 +0300

    clearBss routine added

--- source/runtime/startup.s
+++ source/runtime/startup.s
@@ -4,28 +4,41 @@
 
 .global _init
 _init:
-	; stack setup
-	ldi r20, lo8(RAMEND - 1)
+	eor r1, r1			; set r1 reg as zero, this is for convention
+	ldi r20, lo8(RAMEND - 1)	; stack setup
 	out SPL, r20
 
-	; copy data to RAM
+clearBss:
+	ldi XL, lo8(_sbss)
+	ldi XH, hi8(_sbss)
+	ldi r20, lo8(_ebss)
+	ldi r21, hi8(_ebss)
+	rjmp clearBssStart
+
+clearBssLoop:
+	st X+, r1
+
+clearBssStart:
+	cp  XL, r20
+	cpc XH, r21
+	brlo clearBssLoop
+
+copyData:				; copy data to RAM
 	ldi ZH, hi8(_etext)
 	ldi ZL, lo8(_etext)
 	ldi XH, hi8(_sdata)
 	ldi XL, lo8(_sdata)
-
 	ldi r20, hi8(_edata)
 	ldi r21, lo8(_edata)
-
 	rjmp copyCompare
 
-copyStore:
+copyDataLoop:
 	lpm r0, Z+
 	st X+, r0
 
-copyCompare:
-	cp XH, r20
-	cpc XL, r21
+copyDataStart:
+	cp  XL, r20
+	cpc XH, r21
 	brlo copyStore
 	rcall main	; main is called, here it is entry
 
