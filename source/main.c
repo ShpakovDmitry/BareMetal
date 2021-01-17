81adf7296c35078330072e8851c651c524f920e1
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2021-01-17 16:51:26 +0200

    +)main: added led blink

--- source/main.c
+++ source/main.c
@@ -4,6 +4,37 @@
  *   date: 2020-08-20
  */
 
+#include <stdint.h>
+
+/* GPIO_0 direction set/clear registers; */
+#define GPIO_0_DIRSET (*(volatile uint32_t* )0x50000518u)
+#define GPIO_0_DIRCLR (*(volatile uint32_t* )0x5000051Cu)
+
+/* GPIO_0 output set/clear registers */
+#define GPIO_0_OUTSET (*(volatile uint32_t* )0x50000508u)
+#define GPIO_0_OUTCLR (*(volatile uint32_t* )0x5000050Cu)
+
+#define LED_PIN 13
+
+void delay_loop();
+
 int main(void) {
+    /* LED1 on DK is at GPIO port 0 and pin 13 */
+    /* set this port as output */
+    GPIO_0_DIRSET = 1 << LED_PIN;
+    
+    while (1) {
+        GPIO_0_OUTSET = 1 << LED_PIN;
+        delay_loop();
+        GPIO_0_OUTCLR = 1 << LED_PIN;
+        delay_loop();
+    }
+    
     return 0;
+}
+
+void delay_loop() {
+    for (volatile uint32_t i = 0; i < 0x00100000; ++i ) {
+        ;
+    }
 }
